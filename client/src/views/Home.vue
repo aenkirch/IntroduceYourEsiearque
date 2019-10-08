99dd3be967b385a6b9375104e3e1046d661ffd47
Adel <adelenkirche@pm.me>
2019-10-08 10:42:52 +0200

    Interface de creation de carte integree

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="home">
+  <div class="mainDiv">
     <h1>Welcome {{connectedUserName}}</h1>
+    <input v-model="age" placeholder="Type your age">
+    <input v-model="from" placeholder="What have you done before joining ESIEA Gradute School of Engineering ?">
+    <button class="valider" v-on:click="this.checkForm">Valider</button>
+
+    <div>
+      <ul>
+        <li v-for="item in cards" :key="item.name + item.from">
+          Nom: {{ connectedUserName }} - Age: {{ item.age }} - From: {{ item.from }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -9,6 +20,28 @@ import { mapState, mapMutations, mapActions } from 'vuex'
 
 export default {
   name: 'home',
-  computed: mapState(['connectedUserName'])
+  computed: mapState(['connectedUserName']),
+  data: function() {
+    return {
+      name: '',
+      age: '',
+      from: '',
+      cards: []
+    }
+  },
+  methods: {
+    checkForm (e) {
+      /* eslint-disable */
+
+      e.preventDefault();
+
+      this.cards.push({name: this.name, age: this.age, from: this.from});
+      console.log(e.target);
+      console.log(this.name);
+      console.log(this.age);
+      console.log(this.from);
+      console.log(this.cards);
+    }
+  }
 }
 </script>
